f4b0c8e7d17fbfa1245559f889e168529f7acb69
jaehun <jaehun@vbox.42gyeongsan.kr>
2025-08-19 18:13:24 +0900

    FEAT: WORDPRESS 2USER

--- srcs/requirements/mariadb/tools/mariadb_entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb_entrypoint.sh
@@ -6,6 +6,10 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
+echo "Setting up permissions..."
+chown -R mysql:mysql /var/lib/mysql
+chmod -R 755 /var/lib/mysql
+
 # 임시 init 파일 생성
 INIT_FILE="/tmp/init.sql"
 cat > "$INIT_FILE" << EOF
